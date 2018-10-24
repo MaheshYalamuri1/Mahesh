e9c4afc08871d3e73721ddceb12bff80f52ffcd3
MaheshYalamuri1 <44153151+MaheshYalamuri1@users.noreply.github.com>
2018-10-24 10:18:17 +0530

    Add files via upload

---
+++ BA.java
@@ -0,0 +1,15 @@
+class B 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("MB");
+		int i=0;
+		do
+		{
+			System.out.println("body:"+i);
+			i++;
+		}
+		while (i<5);
+		System.out.println("ME");
+	}
+}
